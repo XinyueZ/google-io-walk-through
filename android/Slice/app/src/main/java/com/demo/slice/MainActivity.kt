3602d39f19b6670e700dbe87ad569c05568dbf86
XinyueZ <xinyue.zhao@immonet.de>
2018-07-17 11:49:37 +0200

    Fix show fragment

--- android/Slice/app/src/main/java/com/demo/slice/MainActivity.kt
+++ android/Slice/app/src/main/java/com/demo/slice/MainActivity.kt
@@ -9,10 +9,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        savedInstanceState?.run {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment::class.newInstance(applicationContext))
-                .commitNow()
-        }
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, MainFragment::class.newInstance(applicationContext))
+            .commitNow()
     }
 }
