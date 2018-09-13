83de8192e9f1c4b6b641ed5f2e6c950d68f789ac
XinyueZ <xinyue.zhao@immonet.de>
2018-09-13 14:47:51 +0200

    Fix starting OCR

--- firebase/MLKit/app/src/main/java/com/github/xinyuez/mlkit/demo/MainActivity.kt
+++ firebase/MLKit/app/src/main/java/com/github/xinyuez/mlkit/demo/MainActivity.kt
@@ -3,12 +3,14 @@ package com.github.xinyuez.mlkit.demo
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(main_appbar)
     }
 
     @Suppress("UNUSED_PARAMETER")
