a4f90c7336fb9e0f1fa848fde0e7135c76cee3ed
XinyueZ <dev.xinyue.zhao@gmail.com>
2018-09-27 00:02:21 +0200

    Refactor Camera.kt

--- firebase/MLKit/app/src/main/java/com/github/xinyuez/mlkit/demo/Camera.kt
+++ firebase/MLKit/app/src/main/java/com/github/xinyuez/mlkit/demo/Camera.kt
@@ -2,14 +2,11 @@ package com.github.xinyuez.mlkit.demo
 
 import io.fotoapparat.configuration.CameraConfiguration
 import io.fotoapparat.selector.LensPositionSelector
-import io.fotoapparat.selector.autoFocus
 import io.fotoapparat.selector.back
-import io.fotoapparat.selector.continuousFocusPicture
 import io.fotoapparat.selector.firstAvailable
-import io.fotoapparat.selector.fixed
 import io.fotoapparat.selector.front
-import io.fotoapparat.selector.highestFps
 import io.fotoapparat.selector.highestResolution
+import io.fotoapparat.selector.highestSensorSensitivity
 import io.fotoapparat.selector.off
 import io.fotoapparat.selector.standardRatio
 import io.fotoapparat.selector.wideRatio
@@ -20,37 +17,25 @@ sealed class Camera(
 ) {
 
     object Back : Camera(
-            lensPosition = back(),
-            configuration = CameraConfiguration(
-                    previewResolution = firstAvailable(
-                            wideRatio(highestResolution()),
-                            standardRatio(highestResolution())
-                    ),
-                    previewFpsRange = highestFps(),
-                    flashMode = off(),
-                    focusMode = firstAvailable(
-                            continuousFocusPicture(),
-                            autoFocus()
-                    ),
-                    frameProcessor = {
-                        // Do something with the preview frame
-                    }
-            )
+        lensPosition = back(),
+        configuration = CameraConfiguration(
+            previewResolution = firstAvailable(
+                wideRatio(highestResolution()),
+                standardRatio(highestResolution())
+            ),
+            sensorSensitivity = highestSensorSensitivity()
+        )
     )
 
     object Front : Camera(
-            lensPosition = front(),
-            configuration = CameraConfiguration(
-                    previewResolution = firstAvailable(
-                            wideRatio(highestResolution()),
-                            standardRatio(highestResolution())
-                    ),
-                    previewFpsRange = highestFps(),
-                    flashMode = off(),
-                    focusMode = firstAvailable(
-                            fixed(),
-                            autoFocus()
-                    )
-            )
+        lensPosition = front(),
+        configuration = CameraConfiguration(
+            previewResolution = firstAvailable(
+                wideRatio(highestResolution()),
+                standardRatio(highestResolution())
+            ),
+            flashMode = off(),
+            sensorSensitivity = highestSensorSensitivity()
+        )
     )
 }
